7ee4351ad116a201e1d69705646c3c4f9564ec8d
judexpower12345 <ogenejude@gmail.com>
2022-09-09 16:47:37 -0700

    my world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,18 @@
 #include <stdio.h>
 
-
-
-/**
- *
- *  * main - Write a C program that prints exactly
- *
- *   * "Programming is like building a multilingual puzzle
- *
- *    * followed by a new line.
- *
- *     * Return: Always 0 (Success)
- *
- *      */
-
+/** 
+ * main - Write a C program that prints exactly
+ * "Programming is like building a multilingual puzzle*
+ * followed by a new line.
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
 
-
-
-			return (0);
-
+		return (0);
 }
+
+
+
+
